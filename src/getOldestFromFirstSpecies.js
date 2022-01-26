8d82628a92fa99293c3eafb305fa046223b458d9
Jacqueline Silva <jacque.sx@hotmail.com>
2022-01-26 20:19:37 -0300

    Requisitos 07/10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.filter((e) => e.id === id);
+  const primeiroAnimal = funcionario.map((e) => e.responsibleFor[0]);
+  const idAnimal = primeiroAnimal.toString();
+
+  const residentss = species.find((e) => e.id === idAnimal);
+  const residentsOrdenados = residentss.residents.sort((a, b) => a.age - b.age);
+  const animalIdoso = residentsOrdenados[residentsOrdenados.length - 1];
+
+  return Object.values(animalIdoso);
 }
 
 module.exports = getOldestFromFirstSpecies;
